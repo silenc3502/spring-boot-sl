b2440444637f2f1d45cf9ef2533584e1407ea866
Sanghoon Lee <gcccompil3r@gmail.com>
2025-07-16 09:08:36 +0900

    init: spring special lecture

---
+++ spring-special-lecture/src/main/java/com/example/springspeciallecture/SpringSpecialLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.springspeciallecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpecialLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSpecialLectureApplication.class, args);
+    }
+
+}
---
+++ spring-special-lecture/src/test/java/com/example/springspeciallecture/SpringSpecialLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springspeciallecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpecialLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
